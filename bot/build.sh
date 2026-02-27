0950b1ebfc299800be61c3c4fc671e47c2e1d1f2
Pedro Santos Neves <10762799+Neves-P@users.noreply.github.com>
2026-02-27 16:49:35 +0100

    Fix path to build_container.sh in build script

--- bot/build.sh
+++ bot/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./build_container.sh -o /scratch/public/software-tarballs -- eb  --robot --experimental --easystack ../easystacks/habrok-2023.01-cvmfs.yml
+./bot/build_container.sh -o /scratch/public/software-tarballs -- eb  --robot --experimental --easystack ../easystacks/habrok-2023.01-cvmfs.yml
