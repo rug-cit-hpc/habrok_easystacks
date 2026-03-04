4f2b406614dfa784e0b2dee0110c0297545e4683
Neves-P <p.m.santos.neves@rug.nl>
2026-03-04 15:57:49 +0100

    Fix easystack path

--- bot/build.sh
+++ bot/build.sh
@@ -4,4 +4,4 @@
 git clone --depth 1 --branch main https://gitrepo.service.rug.nl/cit-hpc/habrok/cit-hpc-easybuild.git
 
 # Start build container
-./bot/build_container.sh -o /scratch/public/software-tarballs -- eb  --robot --experimental --easystack ../easystacks/habrok-2023.01-cvmfs.yml
+./bot/build_container.sh -o /scratch/public/software-tarballs -- eb  --robot --experimental --easystack easystacks/habrok-2023.01-cvmfs.yml
