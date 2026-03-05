7157fdf9b5092aa0af50f8cfd6b6ba05c217af03
Neves-P <p.m.santos.neves@rug.nl>
2026-03-05 16:15:37 +0100

    Include accessory scripts

---
+++ scripts/gpu_support/nvidia/install_cuda_host_injections.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+TOPDIR=$(dirname $(realpath $BASH_SOURCE))
+echo "This script was replaced by the $TOPDIR/install_cuda_and_libraries.sh script. See https://www.eessi.io/docs/site_specific_config/gpu/ for more information" >&2
+exit 1
